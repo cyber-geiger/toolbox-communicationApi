c5b0b62687e363456277fd133e2129cbb0136e00
maurice.meier <maurice.meier@fhnw.ch>
2021-12-23 10:14:06 +0100

    Run in Foreground with intent

--- lib/src/communication/plugin_starter.dart
+++ lib/src/communication/plugin_starter.dart
@@ -7,25 +7,29 @@ import "package:android_intent_plus/android_intent.dart";
 
 class PluginStarter {
   static Future<void> startPlugin(PluginInformation pi) async {
-      //TODO(mgwerder): to be implemented at least for android (intent) and Windows (call)
-      var executables = pi.executable?.split(";");
-      var componentName= executables?.elementAt(0);
-      var package = executables?.elementAt(1);
-      var windowsExecutabele = executables?.elementAt(2);
-      if(Platform.isAndroid){
-        AndroidIntent intent = AndroidIntent(
-            package: package,
-            componentName: componentName
-        );
-        intent.launch();
-      }
-      else if(Platform.isWindows){
-        if(windowsExecutabele != null){
-          Process.run(windowsExecutabele,["/foreground"]);
-        }
-      }
-      else{
-        throw Exception("Platform not yet Supported");
+    //TODO(mgwerder): to be implemented at least for android (intent) and Windows (call)
+    var executables = pi.executable?.split(";");
+    var componentName= executables?.elementAt(0);
+    var package = executables?.elementAt(1);
+    var windowsExecutabele = executables?.elementAt(2);
+    if(Platform.isAndroid){
+      AndroidIntent intent = AndroidIntent(
+          package: package,
+          componentName: componentName
+      );
+      intent.launch();
+    }
+    else if(Platform.isWindows){
+      if(windowsExecutabele != null){
+        Process.run(windowsExecutabele,["/foreground"]);
       }
     }
+    else{
+      throw Exception("Platform not yet Supported");
+    }
   }
+  static Future<void> startPluginInBackground(PluginInformation pi) async {
+
+  }
+}
+

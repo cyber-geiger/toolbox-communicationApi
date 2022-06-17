e72552ab5a5f44fb1ef6a307a715dfb972934a51
Kittitschai Yimvuthikul <kittitschai.yimvuthikul@fhnw.ch>
2022-06-17 09:01:14 +0200

    feat(java/test): add dart test interop

---
+++ java/GeigerApiPluginConnector/src/test/java/eu/cybergeiger/api/DartTest.java
@@ -0,0 +1,66 @@
+package eu.cybergeiger.api;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class DartTest {
+  private static final int testMsTimeout = 30000;
+  private final Map<Method, Process> runners = new HashMap<>();
+
+  private static Method getTestMethod(TestInfo info) {
+    return info.getTestMethod().orElseThrow(
+      () -> new RuntimeException("Could not retrieve test method.")
+    );
+  }
+
+  @BeforeEach
+  public void MaybeExecuteDartPart(TestInfo info) {
+    Method method = getTestMethod(info);
+    String[] className = method.getDeclaringClass().getName().split("\\.");
+    String filePath = "./" + String.join("/", className) + ".dart";
+    boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+
+    Process runner;
+    try {
+      runner = new ProcessBuilder()
+        .command(
+          isWindows ? "cmd.exe" : "sh", isWindows ? "/c" : "-c",
+          "flutter", "test", filePath, "--name", method.getName()
+        )
+        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+        .redirectError(ProcessBuilder.Redirect.INHERIT)
+        .directory(new File("./src/test/dart/"))
+        .start();
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to execute dart counterpart.", e);
+    }
+    runners.put(method, runner);
+  }
+
+  @AfterEach
+  public void MaybeCheckDartResult(TestInfo info) throws TimeoutException {
+    Method method = getTestMethod(info);
+    Process runner = runners.get(method);
+    if (runner == null) return;
+    try {
+      if (!runner.waitFor(testMsTimeout, TimeUnit.MILLISECONDS))
+        throw new TimeoutException("Dart part did not end in time.");
+      if (runner.exitValue() != 0)
+        throw new AssertionError("Dart part did not exit successfully.");
+    } catch (InterruptedException e) {
+      throw new RuntimeException("Dart part result check was interrupted.", e);
+    } finally {
+      runner.destroy();
+      runners.remove(method);
+    }
+  }
+}

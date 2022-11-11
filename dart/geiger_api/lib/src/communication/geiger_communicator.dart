df126625168c6af60e523321968c0ea6de696d87
Kittitschai Yimvuthikul <kittitschai.yimvuthikul@fhnw.ch>
2022-11-11 15:00:35 +0100

    fix(dart): broken plugin start

--- dart/geiger_api/lib/src/communication/geiger_communicator.dart
+++ dart/geiger_api/lib/src/communication/geiger_communicator.dart
@@ -59,8 +59,7 @@ class GeigerCommunicator {
   // Sends a message to a target plugin.
   // iOS: If the target gets killed by the OS, onTimeout is called after 20sec if its a register or active plugin event
   // for any other event its a 5sec timeout
-  Future<void> sendMessage(
-      PluginInformation target, Message message, Function onTimeout) async {
+  Future<void> sendMessage(PluginInformation target, Message message) async {
     final socket =
         await Socket.connect(InternetAddress.loopbackIPv4, target.port);
 
@@ -77,24 +76,18 @@ class GeigerCommunicator {
     // On iOS, no error occurs if the other app gets killed,
     // hence we send a UID after every message sent and expect to receive one within 5sec (or 20sec in case of a register / activate plugin)
     _logger.log(Level.INFO, 'Message sent, waiting for response bytes');
-    var responseReceived = false;
     var timeout = 5000;
     if (message.type == MessageType.registerPlugin ||
         message.type == MessageType.activatePlugin) {
       timeout = 20000;
     }
 
-    // If not response bytes are received after a certain time, call the on timeout function
-    Future.delayed(Duration(milliseconds: timeout), (() {
-      if (!responseReceived) {
-        onTimeout();
-      }
-    }));
-
     final bs = ByteStream(socket);
-    SerializerHelper.castTest(
-        'Message', responseUID, await SerializerHelper.readLong(bs), 1);
-    responseReceived = true;
+    final uid = await SerializerHelper.readLong(bs)
+        .timeout(Duration(milliseconds: timeout), onTimeout: () {
+      throw const SocketException('iOS connection refused detected.');
+    });
+    SerializerHelper.castTest('Message', responseUID, uid, 1);
     socket.destroy();
   }
 }
